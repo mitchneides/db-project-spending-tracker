7c1eb82eb30cde939d6db4b3a7e0ed4453926c86
mitchneides <mitch.neides@gmail.com>
2022-11-30 13:55:04 -0500

    completes validation and create new user

--- src/AppLogin.java
+++ src/AppLogin.java
@@ -21,7 +21,27 @@ public class AppLogin {
         }
     }
 
-    public int userIdentification(Map<Integer, String> allUsers) {
+    public int createUser(Connection conn, String username, Double budget) throws SQLException {
+        String command = "INSERT INTO user (name, monthly_allowance) VALUES ('" + username + "', " + budget + ")";
+        Statement stmt = null;
+        int userID = Integer.MIN_VALUE;
+        try {
+            Statement st = conn.createStatement();
+            int status = st.executeUpdate(command);
+            String command2 = "SELECT max(uid) from user;";
+            ResultSet rs = st.executeQuery(command2);
+            while(rs.next()) {
+                userID = Integer.valueOf(rs.getString(1));
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR: could not insert user into database");
+        } finally {
+            if (stmt != null) { stmt.close(); }
+        }
+        return userID;
+    }
+
+    public int userIdentification(Map<Integer, String> allUsers, Connection conn) throws SQLException {
         while (true) {
             // display users
             for (Integer id : allUsers.keySet()) {
@@ -33,14 +53,50 @@ public class AppLogin {
             System.out.print("Please enter the user ID that matches your username\n" +
                     "(Or enter -1 to create a new user): ");
 
-            Integer uID = Integer.valueOf(userIn.nextLine());
+            Integer uID;
+            try {
+                uID = Integer.valueOf(userIn.nextLine());
+            } catch (Exception e) {
+                System.out.println();
+                System.out.println("Please enter an ID from the list, or enter -1 to create a new user");
+                System.out.println();
+                continue;
+            }
             if (allUsers.keySet().contains(uID)) {
                 return uID;
             } else if (uID == -1) {
                 // create new user
-                continue;
+                System.out.println();
+                System.out.println("Welcome!! We just need a little information from you.");
+                System.out.print("Please enter your name: ");
+                String userName = userIn.nextLine();
+
+                double budget;
+                while (true) {
+                    System.out.print("Please enter your monthly spending allowance (format dollars.cents): ");
+                    try {
+                        budget = Double.valueOf(userIn.nextLine());
+                        break;
+                    } catch (Exception e) {
+                        System.out.println();
+                        System.out.println("Please enter a monetary value formatted as dollars.cents");
+                        System.out.println();
+                        continue;
+                    }
+                }
+                // call function to create a new user (returns int)
+                uID = createUser(conn, userName, budget);
+                if (uID > Integer.MIN_VALUE) {
+                    return uID;
+                } else {
+                    System.out.println("Unable to create a new user at this time. Please try again...");
+                    continue;
+                }
             } else {
-                System.out.println("'" + userIn + "' was not recognized. Please try again...");
+                System.out.println();
+                System.out.println("Input was not recognized as a valid ID. " +
+                                   "Please try again...");
+                System.out.println();
             }
         }
     }
@@ -56,8 +112,7 @@ public class AppLogin {
         }
 
         // ask user to identify themselves
-        int userID = userIdentification(allUsers);
-
+        int userID = userIdentification(allUsers, conn);
         return userID;
     }
 }
