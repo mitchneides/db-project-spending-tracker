95870ceeaeaa73faeb7cc1193a585f2d991f834e
mitchneides <mitch.neides@gmail.com>
2022-11-30 15:46:22 -0500

    writes execution flow

--- src/Main.java
+++ src/Main.java
@@ -35,6 +35,35 @@ public class Main {
         }
     }
 
+    public static int getInputChoice() throws SQLException {
+        int choice = Integer.MAX_VALUE;
+        while(true) {
+            Scanner userIn = new Scanner(System.in);
+            System.out.println();
+            System.out.println("What type of data would you like to input? Choose an option from below");
+            System.out.println();
+            System.out.println("1: Record expense");
+            System.out.println("2: Record consumption");
+            System.out.print("Your choice: ");
+            try {
+                choice = Integer.valueOf(userIn.nextLine());
+            } catch (Exception e) {
+                System.out.println();
+                System.out.println("Please enter one of the specified menu options");
+                continue;
+            }
+            if (choice == 1) {
+                return 1;
+            } else if (choice == 2) {
+                return 2;
+            } else {
+                System.out.println();
+                System.out.println("Please enter one of the specified menu options");
+                continue;
+            }
+        }
+    }
+
     // program entry point
     public static void main(String[] args) throws SQLException {
         // Login and connect to DB
@@ -54,7 +83,35 @@ public class Main {
         while (true) {
             // display menu
             int menuChoice = getMenuChoice();
-            if (menuChoice == 3) {
+            if (menuChoice == 1) {
+                // ask for purchase or consumption input
+                int inputChoice = getInputChoice();
+                if (inputChoice == 1) {
+                    // input purchase
+                    Purchase purchase = new Purchase();
+//                    purchase.start(conn);
+
+
+
+                } else {
+                    // input consumption
+                    Consumption consumption = new Consumption();
+//                    consumption.start(conn);
+
+
+
+                }
+            } else if (menuChoice == 2) {
+                // generate report
+                Report report = new Report();
+//                report.start(conn);
+
+
+
+            } else if (menuChoice == 3) {
+                // exit system
+                System.out.println();
+                System.out.println("********* Thanks for stopping by :) *********");
                 break;
             }
         }
